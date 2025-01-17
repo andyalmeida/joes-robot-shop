212fcf1f67c6ef9d2f56da072749d3fbc835e71a
André Almeida <andalvalmeida@gmail.com>
2025-01-17 08:42:06 -0300

    Create site header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from "./home/home.component";
+import { SiteHeaderComponent } from "./site-header/site-header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HomeComponent]
+  imports: [HomeComponent, SiteHeaderComponent]
 })
 export class AppComponent {
 }
