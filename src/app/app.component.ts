42f3e56af72c9a1f38d4eb75c3531a2e111b6535
André Almeida <andalvalmeida@gmail.com>
2025-01-17 09:03:10 -0300

    Create catalog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from "./home/home.component";
 import { SiteHeaderComponent } from "./site-header/site-header.component";
+import { CatalogComponent } from "./catalog/catalog.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HomeComponent, SiteHeaderComponent]
+  imports: [HomeComponent, SiteHeaderComponent, CatalogComponent]
 })
 export class AppComponent {
 }
