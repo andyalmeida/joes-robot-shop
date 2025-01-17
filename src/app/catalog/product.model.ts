54c1356a1ad050f7cac720cc9cf755266be335ea
André Almeida <andalvalmeida@gmail.com>
2025-01-17 11:02:55 -0300

    Implement product model and update catalog component with product details and image handling

---
+++ src/app/catalog/product.model.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    imageName: string;
+    category: string;
+    price: number;
+    discount: number;
+}
+
+
