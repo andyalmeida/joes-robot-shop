05d167caadb901a9fa84e84d74f189a1b6d03bf9
André Almeida <andalvalmeida@gmail.com>
2025-01-17 08:30:36 -0300

    Create home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [HomeComponent]
 })
 export class AppComponent {
 }
