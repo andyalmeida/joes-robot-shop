273db3557079dcb93e18bb9a06633755c35ecbac
André Almeida <andalvalmeida@gmail.com>
2025-01-21 20:09:49 -0300

    Refactor cart functionality: implement CartComponent and service, update app structure, and remove legacy code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HomeComponent } from "./home/home.component";
 import { SiteHeaderComponent } from "./site-header/site-header.component";
 import { CatalogComponent } from "./catalog/catalog.component";
+import { CartComponent } from "./cart/cart.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HomeComponent, SiteHeaderComponent, CatalogComponent]
+  imports: [HomeComponent, SiteHeaderComponent, CatalogComponent, CartComponent]
 })
 export class AppComponent {
 }
